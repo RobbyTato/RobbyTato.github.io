57a3f08a64800e4276f6b1478528f231def0c491
Jacob <ebiljacob99@gmail.com>
2021-01-11 21:15:36 +0300

    Testing

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,14 @@
 $.getJSON('https://ipapi.co/json/', function(data) {
-  console.log(JSON.stringify(data, null, 2));
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb+srv://dbMalBot:emil1999@websitedatabase.0xffv.mongodb.net/IP?retryWrites=true&w=majority";
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("IP");
+    dbo.collection("IPS").insertMany(JSON.stringify(data, null, 2), function(err, res) {
+      if (err) throw err;
+      console.log("It worked!");
+      db.close();
+    });
+  });
+  // console.log(JSON.stringify(data, null, 2));
 });
